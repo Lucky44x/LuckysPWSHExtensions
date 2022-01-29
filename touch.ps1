ad49fbf18e5087fd957ec4c2153efa3938af22e5
Nick Balischewski <nickbali06@gmail.com>
2022-01-30 00:01:36 +0100

    added -dir (Directory) and -con (Content) to the touch command

--- touch.ps1
+++ touch.ps1
@@ -1,3 +1,27 @@
 function touch {
-    set-content -Path ($args[0]) -Value ($null)
+    $global:dir = "~"
+
+    if($args.length -eq 0){
+        Write-Host "Please state a file/folder name"
+        return
+    }
+
+    $content = ""
+    $isDir = $false
+
+    for($i=0; $i -lt $args.Length; $i++){
+        switch($args[$i]){
+            "-dir"{$isDir = $true}
+            "-con"{$content = $args[$i+1]}
+        }
+    }
+
+    if($isDir -eq $false){
+        set-content -Path ($args[0]) -Value ($content)
+    }
+    else{
+        new-item -Path ($args[0]) -ItemType Directory
+    }
+
+    $global:dir = $args[0]
 }
